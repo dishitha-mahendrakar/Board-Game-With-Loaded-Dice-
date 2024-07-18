2b1be64a3452192804dbaf263957726f7465d8dc
dishitha-mahendrakar <141650870+dishitha-mahendrakar@users.noreply.github.com>
2024-07-18 17:52:01 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,11 +1,25 @@
-import random
-
 class Board:
-    def __init__(self, squares):
-        self.squares = squares
+    def __init__(self):
+        self.board_layout = [
+            ['H', 'B', 'H', 'J'],
+            ['H', ' ', ' ', 'B'],
+            ['J', ' ', ' ', 'H'],
+            ['H', 'H', 'B', 'B']
+        ]
+        self.positions = [
+            [1, 2, 3, 4],
+            [12, ' ', ' ', 5],
+            [11, ' ', ' ', 6],
+            [10, 9, 8, 7]
+        ]
 
-    def roll_dice(self):
-        roll = random.choices([1, 2, 3, 'RollAgain'], [0.4, 0.2, 0.2, 0.2])[0]
-        while roll == 'RollAgain':
-            roll = random.choices([1, 2, 3, 'RollAgain'], [0.4, 0.2, 0.2, 0.2])[0]
-        return roll
+    def display_board(self, players):
+        for i in range(len(self.positions)):
+            for j in range(len(self.positions[i])):
+                for player in players:
+                    if player.position == self.positions[i][j]:
+                        print(f"{player.name[0]}", end=" ")
+                        break
+                else:
+                    print(self.board_layout[i][j], end=" ")
+            print()
