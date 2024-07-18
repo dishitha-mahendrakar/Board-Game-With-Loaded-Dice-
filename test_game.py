da66e090d5f3f679383e3edb462579e75b16d708
dishitha-mahendrakar <141650870+dishitha-mahendrakar@users.noreply.github.com>
2024-07-18 17:56:23 +0530

    Create test_game.py

---
+++ test_game.py
@@ -0,0 +1,29 @@
+import unittest
+from game import Game
+from player import Player
+from board import Board
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Game("Player1", "Player2")
+
+    def test_initialization(self):
+        self.assertIsInstance(self.game.board, Board)
+        self.assertIsInstance(self.game.player1, Player)
+        self.assertIsInstance(self.game.player2, Player)
+        self.assertEqual(self.game.current_player, self.game.player1)
+
+    def test_switch_player(self):
+        initial_player = self.game.current_player
+        self.game.switch_player()
+        self.assertNotEqual(self.game.current_player, initial_player)
+
+    def test_check_and_print_win_conditions(self):
+        self.game.player1.balance = 150
+        self.assertTrue(self.game.check_and_print_win_conditions())
+
+        # Add more tests for other methods as needed
+
+if __name__ == "__main__":
+    unittest.main()
